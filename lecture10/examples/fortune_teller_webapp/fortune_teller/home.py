f00f33057f67f945f1ac77b2bfde7c236748aa86
Aman Gupta <gupta.aman@gmail.com>
2018-06-27 13:50:04 -0700

    tiny refactor for showing image on home page of fortune teller web app

--- lecture10/examples/fortune_teller_webapp/fortune_teller/home.py
+++ lecture10/examples/fortune_teller_webapp/fortune_teller/home.py
@@ -3,10 +3,8 @@ from fortune_teller import app
 from fortune_teller import forms
 
 @app.route('/')
-@app.route('/index')
 def hello():
     form = forms.NameForm()
     return render_template('home.html',
-                           image_path=url_for('static', filename='cookie_rich.jpg'),
                            form=form)
 
