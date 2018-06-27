ac803b88629cdb4b265cfab81792f4ea0a163954
Aman Gupta <gupta.aman@gmail.com>
2018-06-27 08:38:49 -0700

    Refactor the directory structure for examples

--- lecture10/examples/fortune_teller_webapp/fortune_teller/__init__.py
+++ lecture10/examples/fortune_teller_webapp/fortune_teller/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'very-hard-password'
-app.config['fortunes_filepath'] = 'fortune_teller_webapp/resources/fortunes.txt'
+app.config['fortunes_filepath'] = 'fortune_teller/resources/fortunes.txt'
 
 fortune_list = []
 with open(app.config['fortunes_filepath'], encoding='utf-8') as f:
