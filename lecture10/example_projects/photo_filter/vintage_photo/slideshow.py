0434be408460d94b2ea04731080151f053d0a2df
Aman Gupta <aman.gupta@inmobi.com>
2018-07-09 22:48:43 -0700

    Add 404 page for slideshow not found in the photo filter  webapp

--- lecture10/example_projects/photo_filter/vintage_photo/slideshow.py
+++ lecture10/example_projects/photo_filter/vintage_photo/slideshow.py
@@ -1,6 +1,7 @@
+import os
 from flask import render_template, url_for
 
-from vintage_photo import app, get_save_folder, get_original_image_path, filters
+from vintage_photo import app, get_original_image_path, filters
 
 class ImageDetails:
     def __init__(self, url, caption):
@@ -10,6 +11,11 @@ class ImageDetails:
 
 @app.route('/slideshow/<image_type>/<hash_id>')
 def slideshow(image_type, hash_id):
+    # first check if this slideshow exists:
+    original_image_path = get_original_image_path(hash_id, image_type)
+    if not os.path.isfile(original_image_path):
+        return render_template('slideshow_not_found.html'), 404
+
     slideshow_images = [ImageDetails(
         url_for('show_image', hash_id=hash_id,
                 filename="original." + image_type), "Original picture")]
